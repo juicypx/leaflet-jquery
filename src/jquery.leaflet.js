0be09539df9cd27bd6550a0f94a429b2c5f4afa1
nkrl <niklas@nkrl.tk>
2017-02-20 01:20:06 +0100

    Use attribute settings for lat, long, and zoom, add TODOs

--- src/jquery.leaflet.js
+++ src/jquery.leaflet.js
@@ -1,9 +1,11 @@
 (function($) {
 $.fn.leaflet = function(options) {
-	var settings = $.extend({}, { // TODO extend leaflet options
+	var settings = $.extend({}, { // TODO extend leaflet options, calculate attribute name from the leaflet option name
 		attributeLatitude: "data-lat",
 		attributeLongitude: "data-long",
 		attributeZoom: "data-zoom",
+		attributeMinZoom: "data-minzoom",
+		attributeMaxZoom: "data-maxzoom",
 		attributeTileLayers: "data-tilelayers",
 		attributeControls: "data-controls",
 		tileLayers: [{
@@ -13,15 +15,16 @@ $.fn.leaflet = function(options) {
 		],
 		controls: [{
 
-		}],
+		}], // TODO implement: iterate through this, if it's an object, "name" is member name and other components are options, if it's a string it's the member name. for each control, parse data-:name-:option attributes
 		zoom: 18,
 		imagePath: "images/maps"
 	}, options);
 
 	return this.each(function() {
-		var lat = $(this).attr("data-lat");
-		var long = $(this).attr("data-long");
-		var zoom = $(this).attr("data-zoom") || settings.zoom;
+		var lat = $(this).attr(settings.attributeLatitude); // TODO if this attribute doesn't exist, look for this attribute at parent elements for ease of use
+		var long = $(this).attr(settings.attributeLongitude);
+		var zoom = $(this).attr(settings.attributeZoom) || settings.zoom;
+		// TODO refactor to option("data-zoom", settings.zoom); requiredOption(settings.attributeLatitude); the latter should throw an error if missing
 
 		var map = L.map(this).setView([lat, long], zoom);
 
