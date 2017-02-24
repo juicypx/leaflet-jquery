4b67f6edba41324f3f59532cca13d7619ed19897
nkrl <niklas@nkrl.tk>
2017-02-24 01:13:32 +0100

    Load controls dynamically according to options

--- src/jquery.leaflet.js
+++ src/jquery.leaflet.js
@@ -14,9 +14,7 @@ $.fn.leaflet = function(options) {
 				attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
 			},
 		],
-		controls: [{
-
-		}], // TODO implement: iterate through this, if it's an object, "name" is member name and other components are options, if it's a string it's the member name. for each control, parse data-:name-:option attributes
+		controls: [], // TODO implement: iterate through this, if it's an object, "name" is member name and other components are options, if it's a string it's the member name. for each control, parse data-:name-:option attributes
 		zoom: 18,
 		imagePath: "leaflet/images"
 	}, options);
@@ -36,7 +34,9 @@ $.fn.leaflet = function(options) {
 
 		L.Icon.Default.imagePath = settings.imagePath;
 		L.marker([lat, long]).addTo(map);
-		L.control.scale().addTo(map);
+		settings.controls.forEach(function(control) {
+			L.control[control]().addTo(map);
+		});
 	});
 	// TODO make marker and controls separate method to chain after map
 	// TODO decorate leaflet https://blog.engineyard.com/2015/7-patterns-refactor-javascript-decorators
